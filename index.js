caaf3e4eebf623edac4e3d2bc907d1d5e11548f2
olympia <owojcik7@gmail.com>
2019-12-01 17:21:29 -0500

    Add content type and create index and about html files

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const Person = require("./person.js")
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-//Instantiate a Person object from it
-const person1 = new Person("John Doe", 30)
+//Create a new server object, 
 
-person1.greeting();
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    //index page
+    if (req.url === '/') {
+
+        fs.readFile(path.join(__dirname, 'public',
+            'index.html'), (err, content) => {
+                //check for err
+                if (err) throw err;
+                //add content type
+                res.writeHead(200, { 'Content-Type': "text/html" })
+                //can put HTML here, see h1 Home on the webpage
+                res.end(content)
+            })
+    }
+
+    //about page
+
+    if (req.url === '/about') {
+
+        fs.readFile(path.join(__dirname, 'public',
+            'about.html'), (err, content) => {
+                //check for err
+                if (err) throw err;
+                //add content type
+                res.writeHead(200, { 'Content-Type': "text/html" })
+                //can put HTML here, see h1 Home on the webpage
+                res.end(content)
+            })
+    }
+})
+
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
